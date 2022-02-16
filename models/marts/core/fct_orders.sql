9d50745a99e9cc477919b9bd95d8c1e9d5476bc3
zineb.saim@talan.com <zineb.saim@talan.com>
2022-02-16 15:31:33 +0000

    commit orders

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,18 +1,30 @@
-with 
-payment as ( 
-    select * from {{ref('stg_payments')}}
-    ),
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
 
-orders as (select * from {{ref('stg_orders')}}),
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
 
-final as
-(
-    select orders.order_ID,
-    orders.customer_id, 
-    payment.amount 
-    
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
     from orders
-
-left join payment using(order_id) 
+    left join order_payments using (order_id)
 )
+
 select * from final
